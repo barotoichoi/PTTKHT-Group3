c7161faba7b2138e59b71796e87a33f7f4c14ad7
barotoichoi <havanbao4510@gmail.com>
2026-07-13 11:08:39 +0700

    1

--- server.js
+++ server.js
@@ -91,6 +91,53 @@ app.get("/api/students/count", async (req, res) => {
   }
 });
 
+//get all users
+// Get all users
+app.get("/users", async (req, res) => {
+  try {
+    const result = await sql.query(`
+      SELECT
+          UserID,
+          FullName,
+          Email,
+          Role,
+          Status
+      FROM Users
+      ORDER BY UserID
+    `);
+
+    res.json(result.recordset);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+});
+
+//get 1 users
+// Get user by ID
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const result = await sql.query`
+      SELECT *
+      FROM Users
+      WHERE UserID = ${id}
+    `;
+
+    if (result.recordset.length === 0) {
+      return res.status(404).json({
+        message: "User not found",
+      });
+    }
+
+    res.json(result.recordset[0]);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err.message);
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
